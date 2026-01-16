deaa5ebba7801909c97a7a9e5d2f1651862f8ec3
Geoff Riley <geoffr@adaso.com>
2026-01-16 20:04:50 +0000

    Extracted command logic to a separate file and introduced REPL function. Added IntelliJ project configuration files to .gitignore.

--- repl.go
+++ repl.go
@@ -1,6 +1,34 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var commandIndex map[string]cliCommand
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		err := scanner.Scan()
+		if !err {
+			fmt.Println("Done.")
+			return
+		}
+		inp := scanner.Text()
+		words := cleanInput(inp)
+		command := words[0]
+		ctrl, ok := commandIndex[command]
+		if ok {
+			_ = ctrl.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	var words []string
@@ -11,4 +39,23 @@ func cleanInput(text string) []string {
 	return words
 }
 
+func init() {
+	commandIndex = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
